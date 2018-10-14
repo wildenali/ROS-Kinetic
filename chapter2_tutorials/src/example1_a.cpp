672bc10bbb438677415c73e3e9c123c8ce3e2f9e
wildenali <wildeeeeen@gmail.com>
2018-10-14 12:43:15 +0700

    ini komit pertama saya

--- chapter2_tutorials/src/example1_a.cpp
+++ chapter2_tutorials/src/example1_a.cpp
@@ -2,7 +2,7 @@
 #include "ros/ros.h"
 #include "std_msgs/String.h"
 #include <sstream>
-
+// chatterCallback
 int main(int argc, char **argv) {
   /* code */
   ros::init(argc, argv, "example1_a");
@@ -20,3 +20,4 @@ int main(int argc, char **argv) {
   }
   return 0;
 }
+// test github
